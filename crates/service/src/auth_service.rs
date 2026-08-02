832825de98c4e1078184f0a0e6a9a1bd6f6a2a59
rosselvert <jhendrapeking9@gmail.com>
2026-08-02 10:12:38 +0700

    feat: create auth API

--- crates/service/src/auth_service.rs
+++ crates/service/src/auth_service.rs
@@ -1,10 +1,40 @@
 use sqlx::PgPool;
+use bcrypt::{ verify, hash, DEFAULT_COST};
 
-pub async fn register(pool: &PgPool, full_name: String, email: String, password: String) -> Result<sqlx::postgres::PgQueryResult, sqlx::Error> {
-    sqlx::query_as!(
-        User,
-        r#"INSERT INTO users (email, full_name, password) VALUES ($1, $2, $3)"#, email, full_name, password
+pub async fn register(pool: &PgPool, full_name: &str, email: &str, password: &str) -> Result<sqlx::postgres::PgQueryResult, sqlx::Error> {
+    let password_hash = hash(password, DEFAULT_COST)
+        .expect("failed to hash password");
+
+    sqlx::query!(
+        r#"INSERT INTO users (email, full_name, password) VALUES ($1, $2, $3)"#
+        , email
+        , full_name
+        , password_hash
     )
     .execute(pool)
     .await
+}
+
+pub async fn login(pool: &PgPool, email: &str, password: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let user = sqlx::query!(
+        r#"SELECT id, email, password FROM users WHERE email = $1"#
+        ,email
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    let user = match user {
+        Some(user) => user,
+        None => {
+            return Err("Invalid email or password".into());
+        }
+    };
+
+    let valid = verify(password, &user.password)?;
+
+    if !valid {
+        return Err("Invalid email or password".into());
+    }
+
+    Ok(())
 }
--- crates/service/src/lib.rs
+++ crates/service/src/lib.rs
@@ -1,3 +1,6 @@
 pub mod user_service;
 pub mod auth_service;
 pub mod product_service;
+
+use user_service::{ get_all_users };
+use auth_service::{ login, register };
